beed36012d0bae36666d013920fb1136fb45c6e8
Renann Prado <darkness.renann@gmail.com>
2015-06-21 20:20:59 -0300

    Loading the json, but still not integrated.

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@
 // creating the angular app
 var chartExampleApp = angular.module("chartExampleApp", ["ngRoute", "ngResource"]);
 
+chartExampleApp.controller("NavController", function navigationController($scope, SalesInfoFactory)
+{
+    SalesInfoFactory.getSalesInformation(123,"asd", "asd2").then(function(response)
+    {
+       console.log(response);        
+    });
+});
+
+chartExampleApp.factory('SalesInfoFactory', function salesInfoFactory($http)
+{
+    var salesInfoFactory =
+    {
+        getSalesInformation: function(year, category, subcategory)
+        {
+            return $http.get("/assets/nets-sales.json");        
+        }
+    };
+    
+    return salesInfoFactory;
+});
